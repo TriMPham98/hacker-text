cc7491d9b628d5ec796165a19e4cd6955b8c5d89
Tri <trimpham98@gmail.com>
2023-04-27 21:56:13 -0700

    Add hacker text hover functionality

--- hacker.js
+++ hacker.js
@@ -1,5 +1,20 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-document.querySelector("h1").onmouseover = event => {
-    event.target.innerText = letters[Math.floor(Math.random() * 26)];
+document.querySelector("h1").onmouseover = (event) => {
+  let iterations = 0;
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iterations) {
+            return event.target.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+    iterations += 1 / 3;
+  }, 30);
 };
